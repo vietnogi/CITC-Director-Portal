c0db92732c83bdae42046041bf3104a6b4f1cdf9
Alex Tran <vietnogi@gmail.com>
2011-10-30 18:21:21 -0700

    updated validate

--- php/lib/validate-v2.php
+++ php/lib/validate-v2.php
@@ -6,7 +6,7 @@ class Validate{
 	}
 	
 	function check ($val, $check, $parameter = NULL) {
-		switch (trim($check)) { 
+		switch (trim($check)) {
 			case 'req':
 				return $this->isMin($val, 1);
 			
@@ -74,38 +74,68 @@ class Validate{
 	}
 
 	public function isAlphaNum ($str) {
-		if (!eregi("^[a-z0-9]+$", $str) && $str != '') {
-			return false;
-		}
-		return true;
-	}
-
-	public function isAlphaNumSym ($str) {
-		if (!eregi("^[a-z0-9_.\-]+$", $str)  && $str != '') {
+		if (!preg_match('/^[a-z0-9]+([\\s]{1}[a-z0-9]|[a-z0-9])+$/i', $str) && $str != '') {
 			return false;
 		}
 		return true;
 	}
 	
 	public function isAlpha ($str) {
-		if (!eregi("^[a-z]+$", $str)  && $str != '') {
+		if (!preg_match('/a-z/i', $str)  && $str != '') {
 			return false;
 		}
 		return true;
 	}
 	
-	public function isAlphaSpace ($str) {
-		if (!eregi("^[a-z]+$", $str) && $str != '') {
-			return false;
-		}
-		return true;
-	}
-	
-	public function isEmail ($str) {
-		if (empty($str)) {
+	public function isEmail ($email) {
+		if (empty($email)) {
 			return true;
 		}
-		return eregi("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$", $str);
+		
+		$isValid = true;
+		$atIndex = strrpos($email, '@');
+		if ($atIndex === false) {
+			return false;
+		}
+		
+		$domain = substr($email, $atIndex +1);
+		$local = substr($email, 0, $atIndex);
+		$localLen = strlen($local);
+		$domainLen = strlen($domain);
+		if ($localLen < 1 || $localLen > 64) { // local part length exceeded
+			return false;
+		}
+		if ($domainLen < 1 || $domainLen > 255) { // domain part length exceeded
+			return false;
+		}
+		if ($local[0] == '.' || $local[$localLen-1] == '.') { // local part starts or ends with '.'
+			return false;
+		}
+		if (preg_match('/\\.\\./', $local)) { // local part has two consecutive dots
+			return false;
+		}
+		if (!preg_match('/^[A-Za-z0-9\\-\\.]+$/', $domain)) { // character not valid in domain part
+			return false;
+		}
+		if (preg_match('/\\.\\./', $domain)) { // domain part has two consecutive dots
+			return false;
+		}
+		if (!preg_match('/^(\\\\.|[A-Za-z0-9!#%&`_=\\/$\'*+?^{}|~.-])+$/', str_replace("\\\\","",$local))) {
+			// character not valid in local part unless 
+			// local part is quoted
+			if (!preg_match('/^"(\\\\"|[^"])+"$/', str_replace("\\\\","",$local))) {
+				return false;
+			}
+		}
+		/*
+		dont have check dns
+		if (!(checkdnsrr($domain,"MX") || checkdnsrr($domain,"A"))) {
+			// domain not found in DNS
+			$isValid = false;
+		}
+		*/
+		
+		return true;
 	}
 	
 	public function isMax ($str, $max) {
