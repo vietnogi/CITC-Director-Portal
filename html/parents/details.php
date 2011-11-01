437e97d5f37918815052e0eb1e95b2348dad1151
Rebekah Ocampo <rocampo@eckxmediagroup.com>
2011-11-01 11:14:29 -0700

    [#20319807] html files

--- html/parents/details.php
+++ html/parents/details.php
@@ -1,9 +1,9 @@
-<h2>Customer <?= $this->ld['customer']['first_name'] ?></h2>
+<h2>Parent <?= $this->ld['parent']['first_name'] ?></h2>
 <div>
 	<h3>Detail Information</h3>
 	<div id="detail">
 		<?
-		pr($this->ld['customer']);
+		pr($this->ld['parent']);
 		?>
 		<a href="javascript: void(1)" class="toggle" data-json="{class: 'hidden', selector: '#detail, #edit-detail'}">Edit</a>
 	</div>
@@ -11,30 +11,30 @@
 	<div id="edit-detail" class="hide">
 		<?
 		$form = new emgForm(array_merge(array(
-			'id' => 'customer-info-form'
-			, 'action' =>  $this->actionUrl() . '&amp;customer_id=' . $this->ld['customer']['customer_id']
+			'id' => 'parent-info-form'
+			, 'action' =>  $this->actionUrl() . '&amp;parent_id=' . $this->ld['parent']['parent_id']
 			, 'method' => 'post'
 			, 'class' => 'emg-form val-form columns'
 		) , array()));
 		
-		$form->legend('Customer Information');
+		$form->legend('Parent Information');
 		
 		$form->text(array(
 			'label' => 'First Name'
 			, 'class' => 'req'
-			, 'value' => $this->ld['customer']['first_name']
+			, 'value' => $this->ld['parent']['first_name']
 		));
 		
 		$form->text(array(
 			'label' => 'Last Name'
 			, 'class' => 'req'
-			, 'value' => $this->ld['customer']['last_name']
+			, 'value' => $this->ld['parent']['last_name']
 		));
 		
 		$form->text(array(
 			'label' => 'Email'
 			, 'class' => 'req email'
-			, 'value' => $this->ld['customer']['email']
+			, 'value' => $this->ld['parent']['email']
 		));
 		
 		
@@ -45,8 +45,8 @@
 		<a href="javascript: void(1)" class="toggle" data-json="{class: 'hidden', selector: '#detail, #edit-detail'}">Cancel Edit</a>
 	</div>
 </div>
-<div class="ajax-fill" data-json="{href: '<?= $this->url('/ajax/comments/main') ?>&amp;customer_id=<?= $this->ld['customer']['customer_id'] ?>&amp;for=customer'}"></div>
-<div class="ajax-fill" data-json="{href: '<?= $this->url('/ajax' . $GLOBALS['bc']->path . '/campers/main') ?>&amp;customer_id=<?= $this->ld['customer']['customer_id'] ?>'}"></div>
+<div class="ajax-fill" data-json="{href: '<?= $this->url('/ajax/comments/main') ?>&amp;parent_id=<?= $this->ld['parent']['parent_id'] ?>&amp;for=parent'}"></div>
+<div class="ajax-fill" data-json="{href: '<?= $this->url('/ajax' . $GLOBALS['bc']->path . '/campers/main') ?>&amp;parent_id=<?= $this->ld['parent']['parent_id'] ?>'}"></div>
 
 <h3>Outstanding Balance</h3>
 
--- html/parents/main.php
+++ html/parents/main.php
@@ -1,32 +1,32 @@
-<h2>Customers List</h2>
+<h2>Parents List</h2>
 
 <?
 require DR . '/html-parts/table-actions.php';
 ?>
 
-<table class="search-results customers">
+<table class="search-results parents">
 	<?
 	require DR . '/html-parts/row-headers.php';
 	
 	foreach ($this->ld['rows'] as $i => $row) {
-		// Customer details page
-		$detailsUrl = $this->url($GLOBALS['bc']->path . '/details') . '&amp;customer_id=' . $row['customer_id'];
-		$customerFullName = $row['first_name'] . ' ' . $row['last_name'];
+		// Parent details page
+		$detailsUrl = $this->url($GLOBALS['bc']->path . '/details') . '&amp;parent_id=' . $row['parent_id'];
+		$parentFullName = $row['first_name'] . ' ' . $row['last_name'];
 		?>
 		<tr>
 			<td class="row-count"><?= $i + 1 ?></td>
 			<td class="first-name">
-				<a href="<?= $detailsUrl ?>" title="View Details for <?= $customerFullName ?>">
+				<a href="<?= $detailsUrl ?>" title="View Details for <?= $parentFullName ?>">
 					<?= $row['first_name'] ?>
 				</a>
 			</td>
 			<td class="last-name">
-				<a href="<?= $detailsUrl ?>" title="View Details for <?= $customerFullName ?>">
+				<a href="<?= $detailsUrl ?>" title="View Details for <?= $parentFullName ?>">
 					<?= $row['last_name'] ?>
 				</a>
 			</td>
 			<td class="email">
-				<a href="mailto:<?= $row['email'] ?>" title="Email <?= $customerFullName ?>">
+				<a href="mailto:<?= $row['email'] ?>" title="Email <?= $parentFullName ?>">
 					<?= $row['email'] ?>
 				</a>
 			</td>
