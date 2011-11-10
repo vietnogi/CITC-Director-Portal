ac88fd44a47e7c2618973088a6078202d5af1021
Alex Tran <vietnogi@gmail.com>
2011-11-09 22:45:06 -0700

    [#20522311] working on saving file

--- action/seasons/upload-image.php
+++ action/seasons/upload-image.php
@@ -1,16 +1,29 @@
 <?
 $inputs = array(
 	'season_id' => newInput('season_id', $_GET, 'min 1 int')
+	, 'season_image' => newInput('season_image', $_FILES, array('tmp_path' => 'min 1'))
 );
 $this->redirect = '/seasons/main';
 
+$file = $_FILES[''];
+
 // validate file size
 $maxSize = 3 * 1024 * 1000 * 1000; //MB
+$validMimes = array(
+	'image/jpeg'
+	, 'image/gif'
+	, 'image/png'
+);
+
 if (filesize(DR . $file) > $maxSize) {
 	$error = 'The uploaded file is too big; the max file size is: ' . $maxSize . ' MB';
 }
-/*
-$uploadPath = CLIENTFILES . '/' . $this->systemVars['clent'] . '/seasons';
-move_uploaded_file($_FILES['season_image']['tmp_name'], 
-*/
+else if(!isMime(DR . $file, $validMimes)){
+	$error = 'The file type is invalid, please upload a JPG, GIF or PNG file.';	
+}
+else {
+	$error = NULL;	
+}
+
+
 ?>
--- data/seasons/details.php
+++ data/seasons/details.php
@@ -9,4 +9,41 @@ $this->ld['season'] = $GLOBALS['mysql']->getSingle($query, $values);
 if (empty($this->ld['season'])) {
 	logError('season is empty.');
 }
+
+$_POST = array(
+	'id' => '1'
+	, 'campers' => array(
+		'first_name' => 'alex'
+		, 'last_name' => 'tran'
+		, 'sessions' => array(
+			'1' => array(
+				'date' => '10/15/2011'
+			)
+			, '2' => array(
+				'name' => 'session 2'
+			)
+		)
+	)
+);
+
+$inputs = array(
+	'id' => newInput('id', $_POST, 'min 1 int')
+	, 'campers' => newInput('campers', $_POST, array(
+		'first_name' => 'min 1'
+		, 'last_name' => 'min 1'
+		, 'sessions' => array(
+			'1' => array(
+				'name' => null
+				, 'date' => NULL
+			)
+			, '2' => array(
+				'name' => NULL
+				, 'date' => NULL
+			)
+		)
+	))
+);
+
+print_r($inputs);
+
 ?>
